39f59f4c04c1014d2db818036a07e6aac6588abb
PashcalE2 <74344847+PashcalE2@users.noreply.github.com>
2024-05-10 18:29:49 +0300

    .

--- RSHD_lab3/scripts/step3/load_and_run_last_backup.sh
+++ RSHD_lab3/scripts/step3/load_and_run_last_backup.sh
@@ -1,7 +1,7 @@
 reserve_node='postgres2@pg191';
 last_backup=`ssh ${reserve_node} "ls backups/ | grep backup_at_ | tail -1"`;
 db_copy_dir='yqi56'
-db_put_dir='yqi99'
+db_put_dir='yqi56'
 
 echo "Загрузка резервной копии: ${last_backup}"
 
@@ -14,6 +14,9 @@ mkdir uzb16;
 rsync -avv $reserve_node:~/backups/$last_backup/uzb16/ uzb16 &&
 echo "Директория файлов таблицы 'street' загружена" || 
 echo "Не удалось загрузить директорию файлов таблицы 'street'";
+
+rsync -avv $reserve_node:~/backups/$last_backup/mwd84/ mwd84;
+rsync -avv $reserve_node:~/backups/$last_backup/orw97/ orw97;
 
 pg_ctl start -D $db_put_dir/ || pg_ctl restart -D $db_put_dir/;
 
---
+++ RSHD_lab3/scripts/step4/insert.sql
@@ -0,0 +1,9 @@
+insert into sickorange values (default, 'Step4_SickOrange91238', '04/05/2006');
+insert into sickorange values (default, 'Step4_SickOrange1131', '09/08/2007');
+
+insert into street values (default, 'Step4_Branch1321');
+insert into street values (default, 'Step4_Branch666');
+
+insert into building values (default, 5, 'Step4_Building3212');
+insert into building values (default, 6, 'Step4_Building1232');
+
